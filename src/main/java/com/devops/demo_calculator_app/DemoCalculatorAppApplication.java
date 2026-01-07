f4340670aef173460e3f46f7b48427c0d66c8512
unknown <tusharbajaj200153@gmail.com>
2026-01-07 20:23:44 +0530

    Initial Spring Boot calculator app

---
+++ src/main/java/com/devops/demo_calculator_app/DemoCalculatorAppApplication.java
@@ -0,0 +1,13 @@
+package com.devops.demo_calculator_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCalculatorAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCalculatorAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devops/demo_calculator_app/DemoCalculatorAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.demo_calculator_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCalculatorAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
