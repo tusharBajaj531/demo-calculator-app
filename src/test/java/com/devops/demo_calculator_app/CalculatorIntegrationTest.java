44b9c3211b022d19cf9bf2a29ea9ff56a863c434
unknown <tusharbajaj200153@gmail.com>
2026-01-07 21:04:18 +0530

    add integration testing

---
+++ src/test/java/com/devops/demo_calculator_app/CalculatorIntegrationTest.java
@@ -0,0 +1,27 @@
+package com.devops.demo_calculator_app;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class CalculatorIntegrationTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void testAddEndpoint() {
+        String response = this.restTemplate
+                .getForObject("http://localhost:" + port + "/calculator/add/2/3", String.class);
+
+        assertThat(response).isEqualTo("5");
+    }
+}
